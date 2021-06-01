9957ac4a2f5731e5c95d1e6689d2d9fc20cfc5e5
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-01 13:59:20 +0000

    [Fix] ユーザー詳細画面のURL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
       render "show"
     end
