368434732d6da7b9e2bc1db3ffe4e2f0fc5fd01f
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-01 14:51:28 +0000

    [Fix] バリデーション introduction

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
 attachment :profile_image, destroy: false
 
 validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+validates :introduction, length: { maximum: 50 }
 
 end
