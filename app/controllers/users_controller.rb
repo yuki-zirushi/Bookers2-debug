fc706cce3e788135bfd58e4e07e015edb03438b4
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-01 15:26:07 +0000

    [Fix] render エラーメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      @user = User.find(params[:id])
       render :edit
     end
   end
