32ec660df513abd543f15a910791a57fd4d74d0e
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-01 13:39:31 +0000

    [Fix] サインイン後のリダイレクト先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
